690836e41a3271b370dd139457f5a7e6865caa2a
Tomoyuki Okamoto <toka.8829@gmail.com>
2017-08-22 10:58:57 +0000

    DIVE09-kadai

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
-100.times do |n|
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(email: email,
-               password: password,
-               password_confirmation: password,
-               )
+n = 1
+while n <= 100
+  Blog.create(
+    title: "あああ",
+    content: "hoge",
+    user_id: n
+  )
+  n = n + 1
 end
